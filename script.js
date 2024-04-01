5c1a456d781d100a50aaac47e8e599fc1e46cac1
andiniseptia <andiniseptiandri124@gmail.com>
2024-04-01 21:34:20 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+function showAlert(){
+    alert(Date(),tampil(),show())
+    Date()
+
+    let tanggal = new Date();
+    document.getElementById("hasil").innerHTML = tanggal;
+
+    function tampil(){
+        alert("Untuk booking bisa cek akun sosial media kami yaaa!")
+    }
+
+    function show(){
+        alert("Ig : Nailartjogja ")
+    }
+}
